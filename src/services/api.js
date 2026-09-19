efed264e01ed0c1201df9d8358a3757fb5abe78b
StMaryStGabriel <eotcstl@gmail.com>
2026-09-19 19:14:24 +0300

    Initial commit - Debre Nazareth Finance Admin

--- src/services/api.js
+++ src/services/api.js
@@ -28,7 +28,7 @@ api.interceptors.response.use(
     if (error.response?.status === 401) {
       localStorage.removeItem("adminToken");
 
-      window.location.href = "/admin/login";
+      window.location.href = "/finance/admin/login";
     }
 
     return Promise.reject(error);
